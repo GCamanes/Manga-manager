5d270b642775cb5ae4c42f640ac47547319f504c
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-02-26 09:58:10 +0100

    added better command handling

--- dl_manager.py
+++ dl_manager.py
@@ -1,13 +1,20 @@
+import argparse
 import sys
 from helpers.manga_helper import MangaHelper
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <manga-id>")
-        sys.exit(1)
-
-    manga_id = sys.argv[1]  # Get the manga ID from the command-line argument
-    manga_info = MangaHelper.get_manga_info(manga_id)
-    print(manga_info)
+    # Definition of argument option
+    parser = argparse.ArgumentParser(prog="dl_manager.py")
+    parser.add_argument('--dlmanga', nargs=1,
+                    help='download manga (use manga id as parameter)',
+                    action='store', type=str)
+    
+    # Parsing of command line argument
+    args = parser.parse_args(sys.argv[1:])
+    
+    if args.dlmanga is not None:
+        manga_info = MangaHelper.get_manga_info(args.dlmanga[0])
+        print(manga_info)
+        sys.exit()
 
     
