1784d0ee444fe0e8418b03b2323aa6242aeb3f8d
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-02-26 09:52:10 +0100

    fixed manga info id

--- entities/manga_info.py
+++ entities/manga_info.py
@@ -3,7 +3,7 @@ import re
 class MangaInfo:
     """Represents a manga with its information and list of chapters."""
     def __init__(self, id, title, authors, genres, status, chapters = []):
-        self.id = id,
+        self.id = id
         self.title = title
         self.authors = authors
         self.genres = genres
@@ -12,4 +12,4 @@ class MangaInfo:
 
     def __repr__(self):
         return (f"#### Manga {self.title}\n* id={self.id}\n* authors={self.authors}\n* genres={self.genres}\n"
-                f"* status={self.status}\n* chapters({len(self.chapters)})={self.chapters})")
+                f"* status={self.status}\n* chapters({len(self.chapters)})")
