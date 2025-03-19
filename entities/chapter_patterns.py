df165ab5e2272f7376b918958e96ff386898f7b9
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-03-19 15:39:32 +0100

    added manga_list.json removed prints

--- entities/chapter_patterns.py
+++ entities/chapter_patterns.py
@@ -20,11 +20,4 @@ class ChapterPatterns:
         normalized = re.sub(r'\d', '#', candidate)
         match = re.search(r"(.*[\d#])[^#\d]*$", normalized)
         pattern = match.group(1) if match else normalized
-        if pattern == "#######-######-g#"\
-            or pattern == "#######-page-###" \
-            or pattern == "#######-the-origin-of-obedience-part-#"\
-            or pattern == "#######-chapter-###-v#-#-##"\
-            or pattern == "#######-vol-##-ch-###-#-ch-##-##"\
-            or pattern == "#######-chapter-#v#":
-            print(candidate)
         self.patterns.add(pattern)
