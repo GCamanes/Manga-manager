a2ac462c0363d534f87a160ab50ccbaefc42698b
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-04-10 15:38:07 +0200

    added mangas list doc

---
+++ entities/firebase/manga_doc_light.py
@@ -0,0 +1,37 @@
+class MangaDocLight:
+    """Represents a manga with its information and list of chapters."""
+    def __init__(self, id: str , title: str , cover_path: str, authors: list[str], genres: list[str], status: str , last_chapter: str = None):
+        self.id: str  = id
+        self.title: str  = title
+        self.cover_path: str = cover_path
+        self.authors: list[str] = authors
+        self.genres: list[str] = genres
+        self.status: str  = status
+        self.last_chapter: str = last_chapter
+
+    def __repr__(self):
+        return (f"#### Manga doc {self.title}\n* id={self.id}\n* cover={self.cover_path}\n* authors={self.authors}\n* genres={self.genres}\n"
+                f"* status={self.status}\n* last_chapter({self.last_chapter})")
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "title": self.title,
+            "cover_path": self.cover_path,
+            "authors": self.authors,
+            "genres": self.genres,
+            "status": self.status,
+            "last_chapter": self.last_chapter
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["id"],
+            data["title"],
+            data["cover_path"],
+            data["authors"],
+            data["genres"],
+            data["status"],
+            data["last_chapter"]
+        )
---
+++ entities/firebase/mangas_list_doc.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+from entities.firebase.manga_doc_light import MangaDocLight
+
+class MangasListDoc:
+    """Represents a manga with its information and list of chapters."""
+    def __init__(self, mangas: Optional[List[MangaDocLight]] = None):
+        self.mangas: List[MangaDocLight] = mangas or []
+
+    def __repr__(self):
+        return (f"#### Mangas {self.mangas}")
+
+    def to_dict(self) -> dict:
+        return {
+            "mangas": [manga.to_dict() for manga in self.mangas]
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        mangas = [MangaDocLight.from_dict(manga) for manga in data.get("mangas", [])]
+        return cls(mangas)
+        
+    def add_or_replace(self, new_manga: MangaDocLight):
+        for i, manga in enumerate(self.mangas):
+            if manga.id == new_manga.id:
+                self.mangas[i] = new_manga
+                return
+        self.mangas.append(new_manga)
