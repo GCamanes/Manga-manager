d048d2c1450ad63f7c2216e06fc4fb8666ba500a
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-04-10 16:54:37 +0200

    removed comment on uplaod

--- helpers/firebase_helper.py
+++ helpers/firebase_helper.py
@@ -131,13 +131,13 @@ class FirebaseHelper:
                 try:
                     sys.stdout.write(f"\r\033[K* chapter {chapter_doc.number} ...")
                     sys.stdout.flush()
-                    """for index, page in enumerate(chapter_doc.pages):
+                    for index, page in enumerate(chapter_doc.pages):
                         percent = math.floor((index + 1) * 100 / len(chapter_doc.pages))
                         barIndex = math.floor(percent/10) 
                         bar = "#" * barIndex + " " * (10 - barIndex)
                         self.__upload_file(f"{Constants.general.DL_PATH}/{page}", page)
                         sys.stdout.write(f"\r\033[K* chapter {chapter_doc.number} : [{bar}] {percent}%")
-                        sys.stdout.flush()"""
+                        sys.stdout.flush()
                     chapter_ref = self.store.collection(Constants.firebase.mangas_collection).document(manga_doc.id)\
                         .collection(Constants.firebase.chapters_collection).document(chapter_doc.number)
                     chapter_ref.set(chapter_doc.to_dict())
